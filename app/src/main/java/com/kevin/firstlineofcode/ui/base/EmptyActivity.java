183e5919e1387c612215e8f53deeffd5bb949dae
Kevin Gu <gubaijing2004@163.com>
2016-04-21 18:16:42 +0800

    finish section 8.3.2

--- app/src/main/java/com/kevin/firstlineofcode/ui/base/EmptyActivity.java
+++ app/src/main/java/com/kevin/firstlineofcode/ui/base/EmptyActivity.java
@@ -1,7 +1,12 @@
 package com.kevin.firstlineofcode.ui.base;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -9,11 +14,21 @@ import android.widget.ImageView;
 import com.kevin.firstlineofcode.R;
 import com.kevin.firstlineofcode.ui.util.Constants;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class EmptyActivity extends BaseDoubleClickActivity implements BaseDoubleClickActivity.MyDoubleClickListener {
     private String flg;
     private ImageView mImg_intent1;
     private ImageView mImg_intent2;
     private Button btn_intent5;//intent返回
+
+    private ImageView img_photo;
+
+    public static final int TAKE_PHOTO = 1;
+    public static final int CROP_PHOTO = 2;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,7 @@ public class EmptyActivity extends BaseDoubleClickActivity implements BaseDouble
     private void initViews() {
         mImg_intent1 = (ImageView) findViewById(R.id.img_intent_1);
         mImg_intent2 = (ImageView) findViewById(R.id.img_intent_2);
+        img_photo = (ImageView) findViewById(R.id.img_photo);
         btn_intent5 = (Button) findViewById(R.id.btn_intent_5);
         if("intent5".equals(flg)){
             btn_intent5.setVisibility(View.VISIBLE);
@@ -51,26 +67,72 @@ public class EmptyActivity extends BaseDoubleClickActivity implements BaseDouble
 
     @Override
     public void doDouble() {
-        switch (flg) {
-            case "intent1" ://隐式intent1
-                if(mImg_intent1.getVisibility() == View.VISIBLE){
-                    mImg_intent1.setVisibility(View.INVISIBLE);
-                }else if(mImg_intent1.getVisibility() == View.INVISIBLE){
-                    mImg_intent1.setVisibility(View.VISIBLE);
+        if(null != flg && !flg.isEmpty()){
+            switch (flg) {
+                case "intent1" ://隐式intent1
+                    if(mImg_intent1.getVisibility() == View.VISIBLE){
+                        mImg_intent1.setVisibility(View.INVISIBLE);
+                    }else if(mImg_intent1.getVisibility() == View.INVISIBLE){
+                        mImg_intent1.setVisibility(View.VISIBLE);
+                    }
+                    break;
+                case "intent2" ://隐式intent2
+                    if(mImg_intent2.getVisibility() == View.VISIBLE){
+                        mImg_intent2.setVisibility(View.INVISIBLE);
+                    }else if(mImg_intent2.getVisibility() == View.INVISIBLE){
+                        mImg_intent2.setVisibility(View.VISIBLE);
+                    }
+                    break;
+                case "toPhoto":
+                //创建File对象，用于存储拍照后的图片
+                File outpustImage = new File(Environment.getExternalStorageDirectory(), "tempImage.jpg");
+                if(outpustImage.exists()){
+                    outpustImage.delete();
                 }
-            break;
-            case "intent2" ://隐式intent2
-                if(mImg_intent2.getVisibility() == View.VISIBLE){
-                    mImg_intent2.setVisibility(View.INVISIBLE);
-                }else if(mImg_intent2.getVisibility() == View.INVISIBLE){
-                    mImg_intent2.setVisibility(View.VISIBLE);
+                try {
+                    outpustImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+                imageUri = Uri.fromFile(outpustImage);
+                Intent intentImg = new Intent("android.media.action.IMAGE_CAPTURE");
+                intentImg.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intentImg, TAKE_PHOTO);
                 break;
+            }
+        }else{
+            showToast("不知道你要干嘛！");
         }
     }
 
     @Override
     public void onBackPressed() {
         doBeforeResultReturn();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case TAKE_PHOTO :
+                if(resultCode == RESULT_OK){
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CROP_PHOTO);//启动裁剪程序
+                }
+                break;
+            case CROP_PHOTO:
+                if(resultCode == RESULT_OK){
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        img_photo.setImageBitmap(bitmap);//将裁剪后的照片显示出来
+                        img_photo.setVisibility(View.VISIBLE);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/kevin/firstlineofcode/ui/home/SectionHFragment.java
+++ app/src/main/java/com/kevin/firstlineofcode/ui/home/SectionHFragment.java
@@ -1,14 +1,27 @@
 package com.kevin.firstlineofcode.ui.home;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.kevin.firstlineofcode.R;
+import com.kevin.firstlineofcode.ui.base.BaseBarActivity;
+import com.kevin.firstlineofcode.ui.base.EmptyActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,13 +31,13 @@ import com.kevin.firstlineofcode.R;
  * Use the {@link SectionHFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SectionHFragment extends Fragment {
-//    private OnFragmentInteractionListener mListener;
+public class SectionHFragment extends Fragment implements AdapterView.OnItemClickListener {
+    //    private OnFragmentInteractionListener mListener;
+    private ListView listView;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
      */
     // TODO: Rename and change types and number of parameters
     public static SectionHFragment newInstance() {
@@ -45,7 +58,157 @@ public class SectionHFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_section_8, null);
+        init(root);
         return root;
+    }
+
+    private void init(View root) {
+        listView = (ListView) root.findViewById(R.id.section_8_lv);
+        List<String> list = new ArrayList<String>();
+        list.add("8.1 使用通知：NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);\n" +
+                "接下来需要创建一个Notification 对象，这个对象用于存储通知所需的各种信息，我们\n" +
+                "可以使用它的有参构造函数来进行创建。Notification 的有参构造函数接收三个参数，第一个\n" +
+                "参数用于指定通知的图标，比如项目的res/drawable 目录下有一张icon.png 图片，那么这里\n" +
+                "就可以传入R.drawable.icon。第二个参数用于指定通知的ticker 内容，当通知刚被创建的时\n" +
+                "候，它会在系统的状态栏一闪而过，属于一种瞬时的提示信息。第三个参数用于指定通知被\n" +
+                "创建的时间，以毫秒为单位，当下拉系统状态栏时，这里指定的时间会显示在相应的通知上。\n" +
+                "因此，创建一个Notification 对象就可以写成：\n" +
+                "Notification notification = new Notification(R.drawable.icon, \"This is ticker text\",\n" +
+                "System.currentTimeMillis());\n" +
+                "创建好了Notification 对象后，我们还需要对通知的布局进行设定，这里只需要调用\n" +
+                "Notification 的setLatestEventInfo()方法就可以给通知设置一个标准的布局。这个方法接收四\n" +
+                "个参数，第一个参数是Context，这个没什么好解释的。第二个参数用于指定通知的标题内\n" +
+                "容，下拉系统状态栏就可以看到这部分内容。第三个参数用于指定通知的正文内容，同样下\n" +
+                "拉系统状态栏就可以看到这部分内容。第四个参数我们暂时还用不到，可以先传入null。因\n" +
+                "此，对通知的布局进行设定就可以写成：\n" +
+                "notification.setLatestEventInfo(context, \"This is content title\", \"This is\n" +
+                "content text\", null);\n" +
+                "以上工作都完成之后，只需要调用NotificationManager 的notify()方法就可以让通知显示\n" +
+                "出来了。notify()方法接收两个参数，第一个参数是id，要保证为每个通知所指定的id 都是\n" +
+                "不同的。第二个参数则是Notification 对象，这里直接将我们刚刚创建好的Notification 对象\n" +
+                "传入即可。因此，显示一个通知就可以写成：\n" +
+                "manager.notify(1, notification);\n" +
+                "到这里就已经把创建通知的每一个步骤都分析完了.\n");
+        list.add("上面的方法已经过时，其实是使用如下方法实现的：NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);\n" +
+                "NotificationCompat.Builder ncb = new NotificationCompat.Builder(getContext());\n" +
+                "ncb.setTicker(\"ticker:这是第一个Notifiy例子。\");\n" +
+                "ncb.setContentText(\"contentText:这是Notifiy内容。\");\n" +
+                "ncb.setContentTitle(\"contentTitle:这是Notifiy标题。\");\n" +
+                "ncb.setSmallIcon(R.mipmap.ic_launcher);\n" +
+                "ncb.setAutoCancel(true);\n" +
+                "ncb.setDefaults(Notification.DEFAULT_ALL);\n" +
+                "Intent intent = new Intent(getContext(), EmptyActivity.class);\n" +
+                "PendingIntent pi = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);\n" +
+                "ncb.setContentIntent(pi);\n" +
+                "notificationManager.notify(1, ncb.build());");
+        list.add("，如果我们没有在代码中对该\n" +
+                "通知进行取消，它就会一直显示在系统的状态栏上显示.\n" +
+                "NotificationManager manager = (NotificationManager)\n" +
+                "getSystemService(NOTIFICATION_SERVICE);\n" +
+                "manager.cancel(1);");
+        list.add("8.2.1 接收短信 public class MainActivity extends Activity {\n" +
+                "private TextView sender;\n" +
+                "private TextView content;\n" +
+                "private IntentFilter receiveFilter;\n" +
+                "private MessageReceiver messageReceiver;\n" +
+                "@Override\n" +
+                "protected void onCreate(Bundle savedInstanceState) {\n" +
+                "super.onCreate(savedInstanceState);\n" +
+                "setContentView(R.layout.activity_main);\n" +
+                "sender = (TextView) findViewById(R.id.sender);\n" +
+                "content = (TextView) findViewById(R.id.content);\n" +
+                "receiveFilter = new IntentFilter();\n" +
+                "receiveFilter.addAction(\"android.provider.Telephony.SMS_RECEIVED\");\n" +
+                "messageReceiver = new MessageReceiver();\n" +
+                "registerReceiver(messageReceiver, receiveFilter);\n" +
+                "}\n" +
+                "@Override\n" +
+                "protected void onDestroy() {\n" +
+                "super.onDestroy();\n" +
+                "unregisterReceiver(messageReceiver);\n" +
+                "}" +
+                "class MessageReceiver extends BroadcastReceiver {\n" +
+                "@Override\n" +
+                "public void onReceive(Context context, Intent intent) {\n" +
+                "Bundle bundle = intent.getExtras();\n" +
+                "Object[] pdus = (Object[]) bundle.get(\"pdus\"); // 提取短信消息\n" +
+                "SmsMessage[] messages = new SmsMessage[pdus.length];\n" +
+                "for (int i = 0; i < messages.length; i++) {\n" +
+                "messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);\n" +
+                "}\n" +
+                "String address = messages[0].getOriginatingAddress(); // 获取发\n" +
+                "送方号码\n" +
+                "String fullMessage = \"\";\n" +
+                "for (SmsMessage message : messages) {\n" +
+                "fullMessage += message.getMessageBody(); // 获取短信内容\n" +
+                "}\n" +
+                "sender.setText(address);\n" +
+                "content.setText(fullMessage);\n" +
+                "}\n" +
+                "}\n" +
+                "}");
+        list.add("8.3.2 调用摄像头拍照 public class MainActivity extends Activity {\n" +
+                "public static final int TAKE_PHOTO = 1;\n" +
+                "public static final int CROP_PHOTO = 2;\n" +
+                "private Button takePhoto;\n" +
+                "private ImageView picture;\n" +
+                "private Uri imageUri;\n" +
+                "@Override\n" +
+                "protected void onCreate(Bundle savedInstanceState) {\n" +
+                "super.onCreate(savedInstanceState);\n" +
+                "setContentView(R.layout.activity_main);\n" +
+                "takePhoto = (Button) findViewById(R.id.take_photo);\n" +
+                "picture = (ImageView) findViewById(R.id.picture);\n" +
+                "takePhoto.setOnClickListener(new OnClickListener() {\n" +
+                "@Override\n" +
+                "public void onClick(View v) {\n" +
+                "// 创建File对象，用于存储拍照后的图片\n" +
+                "File outputImage = new File(Environment.\n" +
+                "getExternalStorageDirectory(), \"tempImage.jpg\");\n" +
+                "try {\n" +
+                "if (outputImage.exists()) {\n" +
+                "outputImage.delete();\n" +
+                "}\n" +
+                "outputImage.createNewFile();\n" +
+                "} catch (IOException e) {\n" +
+                "e.printStackTrace();\n" +
+                "}\n" +
+                "imageUri = Uri.fromFile(outputImage);\n" +
+                "Intent intent = new Intent(\"android.media.action. IMAGE_CAPTURE\");\n" +
+                "intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);\n" +
+                "startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序\n" +
+                "}\n" +
+                "});\n" +
+                "}\n" +
+                "@Override\n" +
+                "protected void onActivityResult(int requestCode, int resultCode, Intent data) {\n" +
+                "switch (requestCode) {\n" +
+                "case TAKE_PHOTO:\n" +
+                "if (resultCode == RESULT_OK) {\n" +
+                "Intent intent = new Intent(\"com.android.camera.action.CROP\");\n" +
+                "intent.setDataAndType(imageUri, \"image/*\");\n" +
+                "intent.putExtra(\"scale\", true);\n" +
+                "intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);\n" +
+                "startActivityForResult(intent, CROP_PHOTO); // 启动裁剪程序\n" +
+                "}\n" +
+                "break;\n" +
+                "case CROP_PHOTO:\n" +
+                "if (resultCode == RESULT_OK) {\n" +
+                "try {\n" +
+                "Bitmap bitmap = BitmapFactory.decodeStream\n" +
+                "(getContentResolver()\n" +
+                ".openInputStream(imageUri));\n" +
+                "picture.setImageBitmap(bitmap); // 将裁剪后的照片显示出来\n" +
+                "} catch (FileNotFoundException e) {\n" +
+                "e.printStackTrace();\n" +
+                "}\n" +
+                "}\n" +
+                "break;\n" +
+                "default:\n" +
+                "break;\n" +
+                "}");
+        listView.setAdapter(new MyAdapter5(getContext(), R.layout.listview_layout, list));
+        listView.setOnItemClickListener(this);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -70,6 +233,35 @@ public class SectionHFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
 //        mListener = null;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationCompat.Builder ncb = new NotificationCompat.Builder(getContext());
+                ncb.setTicker("ticker:这是第一个Notifiy例子。");
+                ncb.setContentText("contentText:这是Notifiy内容。");
+                ncb.setContentTitle("contentTitle:这是Notifiy标题。");
+                ncb.setSmallIcon(R.mipmap.ic_launcher);
+                ncb.setAutoCancel(true);
+                ncb.setDefaults(Notification.DEFAULT_ALL);
+                Intent intent = new Intent(getContext(), EmptyActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                ncb.setContentIntent(pi);
+                notificationManager.notify(1, ncb.build());
+                break;
+            //消除状态栏的通知
+            case 3:
+                NotificationManager manager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.cancel(1);
+                break;
+            //启动摄像头
+            case 4:
+                ((BaseBarActivity)getContext()).openActivity(EmptyActivity.class, "toPhoto");
+                break;
+        }
     }
 
     /**
